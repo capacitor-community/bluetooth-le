d4743da2efef67ffbe68ece6fde3684f42f543cf
pwespi <pwespi@pm.me>
2021-01-24 21:28:00 +0100

    test(android): add test for conversion (#30)
    * test(android): add conversion test
    
    * test(android): run test in gitlab workflow
    
    * test(android): make gradlew executable

---
+++ android/src/test/java/com/capacitorjs/community/plugins/bluetoothle/ConversionKtTest.kt
@@ -0,0 +1,28 @@
+package com.capacitorjs.community.plugins.bluetoothle
+
+import junit.framework.TestCase
+
+fun byteArrayOfInts(vararg ints: Int) = ByteArray(ints.size) { pos -> ints[pos].toByte() }
+
+class ConversionKtTest : TestCase() {
+
+    fun testBytesToString() {
+        val input = byteArrayOfInts(0xA1, 0x2E, 0x38, 0xD4, 0x89, 0xC3)
+        val output = bytesToString(input)
+        assertEquals("A1 2E 38 D4 89 C3 ", output)
+    }
+
+    fun testStringToBytes() {
+        val input = "A1 2E 38 D4 89 C3"
+        val output = stringToBytes(input)
+        val expected = byteArrayOfInts(0xA1, 0x2E, 0x38, 0xD4, 0x89, 0xC3)
+        expected.forEachIndexed { index, byte ->
+            assertEquals(byte, output[index])
+        }
+    }
+
+    fun testHexToByte() {
+        assertEquals(0.toByte(), hexToByte("00"))
+        assertEquals(205.toByte(), hexToByte("CD"))
+    }
+}
