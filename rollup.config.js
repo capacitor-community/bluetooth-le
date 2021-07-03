843b4da7b441d784b9c08b6deddb1a275950016b
pwespi <pwespi@pm.me>
2021-07-03 13:34:30 +0200

    chore(rollup): fix config for external module throat

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ export default {
       name: 'capacitorCommunityBluetoothLe',
       globals: {
         '@capacitor/core': 'capacitorExports',
+        'throat': 'throat',
       },
       sourcemap: true,
       inlineDynamicImports: true,
@@ -18,5 +19,5 @@ export default {
       inlineDynamicImports: true,
     },
   ],
-  external: ['@capacitor/core'],
+  external: ['@capacitor/core', 'throat'],
 };
