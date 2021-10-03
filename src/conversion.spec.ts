a9fc2772545ad80cf48487732cba74cf30384973
pwespi <pwespi@pm.me>
2021-10-03 12:48:09 +0200

    docs: add doc strings to conversion methods

--- src/conversion.spec.ts
+++ src/conversion.spec.ts
@@ -31,6 +31,31 @@ describe('dataViewToNumbers', () => {
   });
 });
 
+describe('textToDataView', () => {
+  it('should convert a text to a DataView', () => {
+    const result = textToDataView('Hello world');
+    expect(result.byteLength).toEqual(11);
+    expect(result.byteOffset).toEqual(0);
+    expect(dataViewToHexString(result)).toEqual('48 65 6c 6c 6f 20 77 6f 72 6c 64');
+  });
+});
+
+describe('dataViewToText', () => {
+  it('should convert a DataView to text', () => {
+    const value = hexStringToDataView('48 65 6c 6c 6f 20 77 6f 72 6c 64');
+    const result = dataViewToText(value);
+    expect(result).toEqual('Hello world');
+  });
+});
+
+describe('numberToUUID', () => {
+  it('should convert a 16 bit UUID to a 128 bit UUID string', () => {
+    const value = 0x180d;
+    const result = numberToUUID(value);
+    expect(result).toEqual('0000180d-0000-1000-8000-00805f9b34fb');
+  });
+});
+
 describe('hexStringToDataView', () => {
   it('should convert a hex string to a DataView', () => {
     const value = '00 05 c8';
@@ -57,31 +82,6 @@ describe('dataViewToHexString', () => {
     const value = new DataView(Uint8Array.from([0, 5, 200]).buffer);
     const result = dataViewToHexString(value);
     expect(result).toEqual('00 05 c8');
-  });
-});
-
-describe('textToDataView', () => {
-  it('should convert a text to a DataView', () => {
-    const result = textToDataView('Hello world');
-    expect(result.byteLength).toEqual(11);
-    expect(result.byteOffset).toEqual(0);
-    expect(dataViewToHexString(result)).toEqual('48 65 6c 6c 6f 20 77 6f 72 6c 64');
-  });
-});
-
-describe('dataViewToText', () => {
-  it('should convert a DataView to text', () => {
-    const value = hexStringToDataView('48 65 6c 6c 6f 20 77 6f 72 6c 64');
-    const result = dataViewToText(value);
-    expect(result).toEqual('Hello world');
-  });
-});
-
-describe('numberToUUID', () => {
-  it('should convert a 16 bit UUID to a 128 bit UUID string', () => {
-    const value = 0x180d;
-    const result = numberToUUID(value);
-    expect(result).toEqual('0000180d-0000-1000-8000-00805f9b34fb');
   });
 });
 
--- src/conversion.ts
+++ src/conversion.ts
@@ -1,9 +1,38 @@
+/**
+ * Convert an array of numbers into a DataView.
+ */
 export function numbersToDataView(value: number[]): DataView {
   return new DataView(Uint8Array.from(value).buffer);
 }
 
+/**
+ * Convert a DataView into an array of numbers.
+ */
 export function dataViewToNumbers(value: DataView): number[] {
   return Array.from(new Uint8Array(value.buffer));
+}
+
+/**
+ * Convert a string into a DataView.
+ */
+export function textToDataView(value: string): DataView {
+  return numbersToDataView(value.split('').map((s) => s.charCodeAt(0)));
+}
+
+/**
+ * Convert a DataView into a string.
+ */
+export function dataViewToText(value: DataView): string {
+  return String.fromCharCode(...dataViewToNumbers(value));
+}
+
+/**
+ * Convert a 16 bit UUID into a 128 bit UUID string
+ * @param value number, e.g. 0x180d
+ * @return string, e.g. '0000180d-0000-1000-8000-00805f9b34fb'
+ */
+export function numberToUUID(value: number): string {
+  return `0000${value.toString(16)}-0000-1000-8000-00805f9b34fb`;
 }
 
 export function hexStringToDataView(value: string): DataView {
@@ -24,23 +53,6 @@ export function dataViewToHexString(value: DataView): string {
       return s;
     })
     .join(' ');
-}
-
-export function textToDataView(value: string): DataView {
-  return numbersToDataView(value.split('').map((s) => s.charCodeAt(0)));
-}
-
-export function dataViewToText(value: DataView): string {
-  return String.fromCharCode(...dataViewToNumbers(value));
-}
-
-/**
- * Convert a 16 bit UUID to a 128 bit UUID string
- * @param value number, e.g. 0x180d
- * @return string, e.g. '0000180d-0000-1000-8000-00805f9b34fb'
- */
-export function numberToUUID(value: number): string {
-  return `0000${value.toString(16)}-0000-1000-8000-00805f9b34fb`;
 }
 
 export function webUUIDToString(uuid: string | number): string {
