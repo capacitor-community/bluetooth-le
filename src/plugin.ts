fd5736cf26d378211dbaddcd126db5ebf9955afb
pwespi <pwespi@pm.me>
2022-05-01 10:56:28 +0200

    refactor(web): refactor event listeners and plugin initialisation

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,5 +3,5 @@ import { registerPlugin } from '@capacitor/core';
 import type { BluetoothLePlugin } from './definitions';
 
 export const BluetoothLe = registerPlugin<BluetoothLePlugin>('BluetoothLe', {
-  web: () => import('./web').then((m) => m.BluetoothLe),
+  web: () => import('./web').then((m) => new m.BluetoothLeWeb()),
 });
