7f2352d9815913935f2b5877512231767104c248
pwespi <pwespi@pm.me>
2023-03-28 20:58:39 +0200

    chore: rename parseUUID function

--- src/validators.spec.ts
+++ src/validators.spec.ts
@@ -1,30 +1,30 @@
 import { numberToUUID } from './conversion';
-import { validateUUID } from './validators';
+import { parseUUID } from './validators';
 
 describe('Validate UUID', () => {
   const hr = '0000180d-0000-1000-8000-00805f9b34fb';
   const hrUpper = '0000180D-0000-1000-8000-00805F9B34FB';
 
   it('should return lowercase UUID', () => {
-    const output = validateUUID(hr);
+    const output = parseUUID(hr);
     expect(output).toBe(hr);
   });
 
   it('should transform uppercase to lowercase UUID', () => {
-    const output = validateUUID(hrUpper);
+    const output = parseUUID(hrUpper);
     expect(output).toBe(hr);
   });
 
   it('should throw an error for a number', () => {
-    expect(() => validateUUID(0x180d)).toThrowError('type number');
+    expect(() => parseUUID(0x180d)).toThrowError('type number');
   });
 
   it('should validate a transformed number', () => {
-    const output = validateUUID(numberToUUID(0x180d));
+    const output = parseUUID(numberToUUID(0x180d));
     expect(output).toBe(hr);
   });
 
   it('should throw an error for a uuid missing a dash', () => {
-    expect(() => validateUUID(hr.replace('-', ''))).toThrowError('format');
+    expect(() => parseUUID(hr.replace('-', ''))).toThrowError('format');
   });
 });
