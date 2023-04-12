3b6baee958b7b100923caff50046e134ae9cd1b3
aadito123 <aaditolkar123@gmail.com>
2023-04-12 12:16:07 +0800

    added error handling for queue

--- src/queue.ts
+++ src/queue.ts
@@ -1,10 +1,14 @@
 const makeQueue = (): (<T>(fn: () => Promise<T>) => Promise<T>) => {
   let currentTask: Promise<unknown> = Promise.resolve();
-  return (fn) => {
-    const task = currentTask.then(() => fn());
-    currentTask = task;
-    return task;
-  };
+  // create a new promise so that errors can be bubbled
+  // up to the caller without being caught by the queue
+  return (fn) =>
+    new Promise((resolve, reject) => {
+      currentTask = currentTask
+        .then(() => fn())
+        .then(resolve)
+        .catch(reject);
+    });
 };
 
 type Queue = <T>(fn: () => Promise<T>) => Promise<T>;
