6e24941ff05fab525a3b1d9003b1ad5e2e2cd2d7
pwespi <pwespi@pm.me>
2023-04-15 08:57:26 +0200

    test: extend queue test

--- src/queue.spec.ts
+++ src/queue.spec.ts
@@ -1,5 +1,11 @@
 import { getQueue } from './queue';
 
+async function sleep(time: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
 describe('queue', () => {
   let sequenceOfEvents: string[] = [];
 
@@ -27,5 +33,41 @@ describe('queue', () => {
     const queue = getQueue(true);
     await Promise.all([queue(() => job('a')), queue(() => job('b')), queue(() => job('c'))]);
     expect(sequenceOfEvents).toEqual(['a started', 'a done', 'b started', 'b done', 'c started', 'c done']);
+  });
+
+  it('should bubble up promise rejections without breaking the queue', async () => {
+    expect.assertions(2);
+    const queue = getQueue(true);
+    try {
+      await Promise.all([
+        queue(() => job('a')),
+        queue(() => {
+          return Promise.reject('failed');
+        }),
+        queue(() => job('c')),
+      ]);
+    } catch (error) {
+      expect(error).toEqual('failed');
+    }
+    await sleep(50);
+    expect(sequenceOfEvents).toEqual(['a started', 'a done', 'c started', 'c done']);
+  });
+
+  it('should bubble up errors without breaking the queue', async () => {
+    expect.assertions(2);
+    const queue = getQueue(true);
+    try {
+      await Promise.all([
+        queue(() => job('a')),
+        queue(async () => {
+          throw new Error('failed');
+        }),
+        queue(() => job('c')),
+      ]);
+    } catch (error) {
+      expect((error as Error).message).toEqual('failed');
+    }
+    await sleep(50);
+    expect(sequenceOfEvents).toEqual(['a started', 'a done', 'c started', 'c done']);
   });
 });
