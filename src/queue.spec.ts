565dc59c5d64bdbca1cd22caf6bc0500179904db
Aadit Olkar <63646058+aadito123@users.noreply.github.com>
2023-04-15 00:25:02 -0700

    fix(deps): remove throat dependency (#521)
    * removed throat dependency
    
    * added error handling for queue
    
    * uninstall throat and run linter
    
    * test: extend queue test
    
    * chore: format files
    
    * test: extend queue test
    
    ---------
    
    Co-authored-by: pwespi <pwespi@pm.me>

--- src/queue.spec.ts
+++ src/queue.spec.ts
@@ -1,5 +1,11 @@
 import { getQueue } from './queue';
 
+async function sleep(time: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
 describe('queue', () => {
   let sequenceOfEvents: string[] = [];
 
@@ -25,7 +31,43 @@ describe('queue', () => {
 
   it('should run jobs in sequence with queue', async () => {
     const queue = getQueue(true);
-    await Promise.all([queue(() => job('a')), queue(() => job('b'))]);
-    expect(sequenceOfEvents).toEqual(['a started', 'a done', 'b started', 'b done']);
+    await Promise.all([queue(() => job('a')), queue(() => job('b')), queue(() => job('c'))]);
+    expect(sequenceOfEvents).toEqual(['a started', 'a done', 'b started', 'b done', 'c started', 'c done']);
+  });
+
+  it('should bubble up promise rejections without breaking the queue', async () => {
+    expect.assertions(2);
+    const queue = getQueue(true);
+    try {
+      await Promise.all([
+        queue(() => job('a')),
+        queue(() => {
+          return Promise.reject('failed');
+        }),
+        queue(() => job('c')),
+      ]);
+    } catch (error) {
+      expect(error).toEqual('failed');
+    }
+    await sleep(50);
+    expect(sequenceOfEvents).toEqual(['a started', 'a done', 'c started', 'c done']);
+  });
+
+  it('should bubble up errors without breaking the queue', async () => {
+    expect.assertions(2);
+    const queue = getQueue(true);
+    try {
+      await Promise.all([
+        queue(() => job('a')),
+        queue(async () => {
+          throw new Error('failed');
+        }),
+        queue(() => job('c')),
+      ]);
+    } catch (error) {
+      expect((error as Error).message).toEqual('failed');
+    }
+    await sleep(50);
+    expect(sequenceOfEvents).toEqual(['a started', 'a done', 'c started', 'c done']);
   });
 });
--- src/queue.ts
+++ src/queue.ts
@@ -1,11 +1,21 @@
-import throat from 'throat';
-
 type Queue = <T>(fn: () => Promise<T>) => Promise<T>;
+
+const makeQueue = (): Queue => {
+  let currentTask: Promise<unknown> = Promise.resolve();
+  // create a new promise so that errors can be bubbled
+  // up to the caller without being caught by the queue
+  return (fn) =>
+    new Promise((resolve, reject) => {
+      currentTask = currentTask
+        .then(() => fn())
+        .then(resolve)
+        .catch(reject);
+    });
+};
 
 export function getQueue(enabled: boolean): Queue {
   if (enabled) {
-    return throat(1);
-  } else {
-    return (fn) => fn();
+    return makeQueue();
   }
+  return (fn) => fn();
 }
