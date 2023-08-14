31fa734a1813812ea4ffff12e7f0054aa7faf589
Andy3189 <andershausding@yahoo.de>
2023-08-14 20:48:22 +0200

    fix(android): fix concurrency issue in timeoutmap(#419) (#560)
    * fix(android): fix concurrency issue in timeoutmap(#419)
    
    * fix(android): rename into TimeoutHandler, cleanup
    
    * fix(android): replace removeIf
    
    ---------
    
    Co-authored-by: Anders Hausding <ahausding@sensorstim.com>

--- android/src/main/java/com/capacitorjs/community/plugins/bluetoothle/Device.kt
+++ android/src/main/java/com/capacitorjs/community/plugins/bluetoothle/Device.kt
@@ -10,11 +10,32 @@ import android.os.Handler
 import android.os.Looper
 import com.getcapacitor.Logger
 import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicReference
 
 class CallbackResponse(
     val success: Boolean,
     val value: String,
 )
+
+class TimeoutHandler(
+    val key: String,
+    val handler: Handler
+)
+
+fun <T> ConcurrentLinkedQueue<T>.popFirstMatch(predicate: (T) -> Boolean): T? {
+    synchronized(this) {
+        val iterator = this.iterator()
+        while (iterator.hasNext()) {
+            val nextItem = iterator.next()
+            if (predicate(nextItem)) {
+                iterator.remove()
+                return nextItem
+            }
+        }
+        return null
+    }
+}
 
 class Device(
     private val context: Context,
@@ -35,7 +56,7 @@ class Device(
     private var device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(address)
     private var bluetoothGatt: BluetoothGatt? = null
     private var callbackMap = HashMap<String, ((CallbackResponse) -> Unit)>()
-    private var timeoutMap = HashMap<String, Handler>()
+    private val timeoutQueue = ConcurrentLinkedQueue<TimeoutHandler>()
     private var bondStateReceiver: BroadcastReceiver? = null
     private var currentMtu = -1
 
@@ -510,20 +531,19 @@ class Device(
     private fun resolve(key: String, value: String) {
         if (callbackMap.containsKey(key)) {
             Logger.debug(TAG, "resolve: $key $value")
+
+            timeoutQueue.popFirstMatch { it.key == key }?.handler?.removeCallbacksAndMessages(null)
             callbackMap[key]?.invoke(CallbackResponse(true, value))
             callbackMap.remove(key)
-            timeoutMap[key]?.removeCallbacksAndMessages(null)
-            timeoutMap.remove(key)
         }
     }
 
     private fun reject(key: String, value: String) {
         if (callbackMap.containsKey(key)) {
             Logger.debug(TAG, "reject: $key $value")
+            timeoutQueue.popFirstMatch { it.key == key }?.handler?.removeCallbacksAndMessages(null)
             callbackMap[key]?.invoke(CallbackResponse(false, value))
             callbackMap.remove(key)
-            timeoutMap[key]?.removeCallbacksAndMessages(null)
-            timeoutMap.remove(key)
         }
     }
 
@@ -531,7 +551,7 @@ class Device(
         key: String, message: String, timeout: Long
     ) {
         val handler = Handler(Looper.getMainLooper())
-        timeoutMap[key] = handler
+        timeoutQueue.add(TimeoutHandler(key, handler))
         handler.postDelayed({
             reject(key, message)
         }, timeout)
@@ -544,7 +564,7 @@ class Device(
         timeout: Long,
     ) {
         val handler = Handler(Looper.getMainLooper())
-        timeoutMap[key] = handler
+        timeoutQueue.add(TimeoutHandler(key, handler))
         handler.postDelayed({
             connectionState = STATE_DISCONNECTED
             gatt?.disconnect()
