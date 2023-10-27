bfed8724966cebe529ba7461c8b5f1fa84644546
Mael Primet <mael.primet@gmail.com>
2023-10-28 00:48:20 +0200

    feat: update naming

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorCommunityBluetoothLe',
+      name: 'maelpBluetoothLe',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
