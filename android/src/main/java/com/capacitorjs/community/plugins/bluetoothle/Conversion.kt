2f25830f9beaf9dd248551ac31bd19ed155c4028
Nate Weibley <nweibley@gmail.com>
2024-10-22 07:45:42 -0500

    Clean up style for consistency

--- android/src/main/java/com/capacitorjs/community/plugins/bluetoothle/Conversion.kt
+++ android/src/main/java/com/capacitorjs/community/plugins/bluetoothle/Conversion.kt
@@ -2,7 +2,7 @@ package com.capacitorjs.community.plugins.bluetoothle
 
 // Create a LUT for high performance ByteArray conversion
 val HEX_LOOKUP_TABLE = IntArray(256) {
-    val hexChars = "0123456789abcdef"
+    val hexChars = "0123456789ABCDEF"
     val h: Int = (hexChars[(it shr 4)].code shl 8)
     val l: Int = hexChars[(it and 0x0F)].code
     (h or l)
@@ -10,12 +10,12 @@ val HEX_LOOKUP_TABLE = IntArray(256) {
 
 // Custom implementation of ByteArray.toHexString until stdlib stabilizes
 private fun ByteArray.toHexString(): String {
-    val result = CharArray(this.size*2);
+    val result = CharArray(this.size * 2);
     var i = 0;
     for (byte in this) {
         val hx = HEX_LOOKUP_TABLE[byte.toInt() and 0xFF]
         result[i]   = (hx shr 8).toChar()
-        result[i+1] = (hx and 0xff).toChar()
+        result[i+1] = (hx and 0xFF).toChar()
         i+=2
     }
     return result.concatToString()
