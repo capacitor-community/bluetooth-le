1fcde9aca8b0c17de50260841b2ee5cc232b8435
Raymond Lian <tkl.development@gmail.com>
2025-02-11 14:12:00 -0500

    Needed to account for int returning for BleClient.read methodfor bluetooth battery service. running convertValue results in an empty object.

--- src/bleClient.ts
+++ src/bleClient.ts
@@ -545,16 +545,16 @@ class BleClientClass implements BleClientInterface {
   }
 
   async read(deviceId: string, service: string, characteristic: string, options?: TimeoutOptions): Promise<DataView> {
+    let batteryServiceUUID = '0000180f-0000-1000-8000-00805f9b34fb';
     service = parseUUID(service);
     characteristic = parseUUID(characteristic);
     const value = await this.queue(async () => {
-      const result = await BluetoothLe.read({
-        deviceId,
-        service,
-        characteristic,
-        ...options,
-      });
-      return this.convertValue(result.value);
+        const result = await BluetoothLe.read(Object.assign({ deviceId,
+            service,
+            characteristic }, options));
+                    
+        if(service != batteryServiceUUID)return this.convertValue(result.value);
+        else return result.value;
     });
     return value;
   }
